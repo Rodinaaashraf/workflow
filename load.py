b7b1860ee503c5cb3c0e2d010ad54e78f8f7f169
Rodina <105175434+Rodinaaashraf@users.noreply.github.com>
2025-05-25 14:16:13 +0300

    Update load.py

--- load.py
+++ load.py
@@ -1,15 +1,19 @@
+import os
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
 def load(df, path="data/output.csv"):
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
     df.to_csv(path, index=False)
     logging.info(f"Data written to {path}")
 
 if __name__ == "__main__":
     from extract import extract
     from transform import transform
+
     data = extract()
     transformed = transform(data)
     load(transformed)
-
